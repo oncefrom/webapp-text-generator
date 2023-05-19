52fa7be2b9b5aa630d0c60d48b67ee24030e1087
umika <442491636@qq.com>
2023-05-19 15:10:49 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,10 +1,10 @@
 import { AppInfo } from "@/types/app"
-export const APP_ID = ''
-export const API_KEY = ''
-export const API_URL = ''
+export const APP_ID = '5fb12581-c2a5-4946-a7f1-555e1621a3be'
+export const API_KEY = 'app-wQ4lSZFHdsPxqddMZbjv47cG'
+export const API_URL = 'https://api.dify.ai/v1'
 export const APP_INFO: AppInfo = {
-  "title": 'Text Generator APP',
-  "description": 'App description',
+  "title": 'lawBot-私人法律助手',
+  "description": '提高法律意识，保障自身权益。根据你的描述生成法律条款建议',
   "copyright": '',
   "privacy_policy": '',
   "default_language": 'zh-Hans'
